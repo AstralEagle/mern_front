f1c340a76097f191c67263e97efad24906994902
antoineRchd <antoine.richard91@gmail.com>
2024-07-05 09:51:56 +0200

    add/delet fav front

--- src/RecipeDetail.jsx
+++ src/RecipeDetail.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import backgroundImage from './img/image3.png'; // Import de l'image de fond
 
 function RecipeDetail() {
   const [searchParams] = useSearchParams();
@@ -31,33 +32,35 @@ function RecipeDetail() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-lg max-w-lg">
-        <img className="w-full h-64 object-cover rounded" src={recette.image} alt={recette.name} />
-        <h2 className="text-2xl font-bold mt-4">{recette.name}</h2>
-        <p className="mt-2">Temps de préparation: {recette.preparation_time}</p>
-        <p>Temps de repos: {recette.rest_time}</p>
-        <p>Temps de cuisson: {recette.cooking_time}</p>
-        <p>Difficulté: {recette.difficulty}</p>
-        <p>Prix: {recette.price}</p>
-        <h3 className="text-xl font-bold mt-4">Ingrédients</h3>
-        <ul className="list-disc list-inside">
-          {recette.ingredients.map((ingredient, index) => (
-            <li key={index}>{ingredient.number} {ingredient.name}</li>
-          ))}
-        </ul>
-        <h3 className="text-xl font-bold mt-4">Instructions</h3>
-        <ol className="list-decimal list-inside">
-          {recette.instructions.map((instruction, index) => (
-            <li key={index} className="mt-1">{instruction}</li>
-          ))}
-        </ol>
-        <button
-          onClick={() => handleAddToFavorites(recette)}
-          className="bg-yellow-400 text-white p-2 mt-4 rounded-lg w-full"
-        >
-          Ajouter aux favoris
-        </button>
+    <div className="min-h-screen bg-cover bg-repeat" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="flex justify-center items-center min-h-screen py-20 ">
+        <div className="bg-orange-50 p-8 rounded-lg shadow-lg max-w-lg">
+          <img className="w-full h-64 object-cover rounded" src={recette.image} alt={recette.name} />
+          <h2 className="text-2xl font-bold mt-4">{recette.name}</h2>
+          <p className="mt-2">Temps de préparation: {recette.preparation_time}</p>
+          <p>Temps de repos: {recette.rest_time}</p>
+          <p>Temps de cuisson: {recette.cooking_time}</p>
+          <p>Difficulté: {recette.difficulty}</p>
+          <p>Prix: {recette.price}</p>
+          <h3 className="text-xl font-bold mt-4">Ingrédients</h3>
+          <ul className="list-disc list-inside">
+            {recette.ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient.number} {ingredient.name}</li>
+            ))}
+          </ul>
+          <h3 className="text-xl font-bold mt-4">Instructions</h3>
+          <ol className="list-decimal list-inside">
+            {recette.instructions.map((instruction, index) => (
+              <li key={index} className="mt-1">{instruction}</li>
+            ))}
+          </ol>
+          <button
+            onClick={() => handleAddToFavorites(recette)}
+            className="bg-yellow-400 text-white p-2 mt-4 rounded-lg w-full"
+          >
+            Ajouter aux favoris
+          </button>
+        </div>
       </div>
     </div>
   );
